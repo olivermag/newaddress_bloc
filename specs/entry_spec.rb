d6d3d84657ed7c1240551e3954700b98114129e9
Olivier Magny <olivier.magny1010@gmail.com>
2015-09-15 17:46:07 -0400

    Fixed typo in entry_spec.rb

--- specs/entry_spec.rb
+++ specs/entry_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Entry do
   context "#to_s" do
     it "prints an entry as a string" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      expected_string = "Name: Ada Lovelace/nPhone Number: 010.012.1815/nEmail: augusta.king@lovelace.com"
+      expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
 
       expect(entry.to_s).to eq(expected_string)
     end
