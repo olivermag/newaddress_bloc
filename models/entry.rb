d3f2e25bad4730e14d23cc233ea3d60a334d1128
Olivier Magny <olivier.magny1010@gmail.com>
2015-09-15 16:58:55 -0400

    Completed menu checkpoint

--- models/entry.rb
+++ models/entry.rb
@@ -8,8 +8,8 @@ class Entry
 	end
 
 	def to_s
-		"Name: #{@name}/nPhone Number: #{@phone_number}/nEmail: #{@email}"
+		"Name: #{@name}\nPhone Number: #{@phone_number}\nEmail: #{@email}"
 	end
-  
-	  
+
+
 end
